f9889d0ed33c5bc5fd346df8660727840aadacb5
delphinep26 <delphine-elfassi@hotmail.com>
2019-05-17 10:34:32 +0300

    new installation

---
+++ db.py
@@ -0,0 +1,8 @@
+# uncompyle6 version 3.3.2
+# Python bytecode 3.6 (3379)
+# Decompiled from: Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)]
+# Embedded file name: C:\Users\Delphine\PycharmProjects\MyProj\db.py
+# Compiled at: 2019-05-14 19:50:54
+# Size of source mod 2**32: 84 bytes
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
---
+++ security.py
@@ -0,0 +1,18 @@
+# uncompyle6 version 3.3.2
+# Python bytecode 3.6 (3379)
+# Decompiled from: Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)]
+# Embedded file name: C:\Users\Delphine\PycharmProjects\MyProj\security.py
+# Compiled at: 2017-10-15 21:42:15
+# Size of source mod 2**32: 344 bytes
+from werkzeug.security import safe_str_cmp
+from resources.user import UserModel
+
+def authenticate(username, password):
+    user = UserModel.find_by_username(username)
+    if user and safe_str_cmp(user.password, password):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
