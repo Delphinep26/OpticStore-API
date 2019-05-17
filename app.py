41159c122269d70e10c752cbfbdf390997f84a6d
delphinep26 <delphine-elfassi@hotmail.com>
2019-05-17 10:05:17 +0300

    First Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+# from flask import Flask
+#
+#
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['PROPAGATE_EXCEPTIONS'] = True
+# app.secret_key = 'jose'
+# api = Api(app)
+
+
+if __name__ == '__main__':
+    #app.run(port=5000, debug=True)
+    print("Test")
