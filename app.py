e7c0086e9a32306c51a6e1ee6582cd121c8ba954
delphinep26 <delphine-elfassi@hotmail.com>
2019-05-17 10:41:08 +0300

    app commit

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-# from flask import Flask
-#
-#
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['PROPAGATE_EXCEPTIONS'] = True
-# app.secret_key = 'jose'
-# api = Api(app)
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.customer import Customer, CustomerList
+from resources.sale import Sale, SaleList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
+api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Customer, '/customer/<int:id>','/customer/<string:first_name>/<string:last_name>')
+api.add_resource(CustomerList, '/customers')
+api.add_resource(Sale, '/sale/<int:id>')
+api.add_resource(SaleList, '/sales')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    #app.run(port=5000, debug=True)
-    print("Test")
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
