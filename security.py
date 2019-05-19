af18a41d08ef09efa393e5784f8dfb7b575496e7
delphinep26 <delphine-elfassi@hotmail.com>
2019-05-20 00:14:45 +0300

    changes

--- security.py
+++ security.py
@@ -1,9 +1,3 @@
-# uncompyle6 version 3.3.2
-# Python bytecode 3.6 (3379)
-# Decompiled from: Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)]
-# Embedded file name: C:\Users\Delphine\PycharmProjects\MyProj\security.py
-# Compiled at: 2017-10-15 21:42:15
-# Size of source mod 2**32: 344 bytes
 from werkzeug.security import safe_str_cmp
 from resources.user import UserModel
 
